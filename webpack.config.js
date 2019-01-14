5def13c5b5b1a7f1e139c4c749ea004c38af6f5a
Lou Stringer <louise.stringer@gmail.com>
2019-01-14 21:12:23 +0000

    set up webpack dev server and html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // puts the node package path into the varable path
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // minimum requirement is entry & output
@@ -11,8 +12,21 @@ module.exports = {
     // path.resove() is a method on the node package path
     // gives access to the __dirname variable -> this contains the path of the current working directory
     // then add the extension to the folder the final bundle should go in
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
   },
-  //mode: 'development'; //makes code faster for development - moved into a script in the package.json
+  // this allows the use of webpack dev server to set up alocal server that updates automatocally when saved
+  devServer: {
+    // the folder where the bundled webpack lives, to give to the server
+    contentBase: './dist'
+  },
+  plugins: [
+    // copies the src index.html into the one in the dist folder
+    new HtmlWebpackPlugin({
+      // the destination file
+      filename: 'index.html',
+      // the file to copy
+      template: './src/index.html'
+    })
+  ]
 }
