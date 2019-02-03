221fa40690c403133d60fdf7f6f43dc863c3d646
Lou Stringer <louise.stringer@gmail.com>
2019-02-03 16:36:33 +0000

    starting the first api calls - cors proxy doesn't work, don't know why

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 // Global app controller
+import axios from 'axios';
+
+// for api requests
+const key = '9dd30cb993b9f387907370655c4d6efe';
+const proxy = 'https://still-reef-35122.herokuapp.com/';
+const searchUrl = 'https://www.food2fork.com/api/search';
+const requestUrl = 'https://www.food2fork.com/api/get';
+
+const getResults = async (query) => {
+  const result = await axios(`${proxy}${searchUrl}?${key}&q=${query}`);
+  console.log(result);
+};
+
+getResults('vegan');
