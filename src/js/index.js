50fe33ea16044167d61cb92127a58b328f38105c
Lou Stringer <louise.stringer@gmail.com>
2019-02-05 21:44:36 +0000

    move cors-anywhere to node_modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 // Global app controller
 import axios from 'axios';
 
-// for api requests
+// for api requests to food2fork
 const key = '9dd30cb993b9f387907370655c4d6efe';
-const proxy = 'https://still-reef-35122.herokuapp.com/';
+// const proxy = 'https://still-reef-35122.herokuapp.com/';
+const proxy = 'https://cors-anywhere.herokuapp.com/';
 const searchUrl = 'https://www.food2fork.com/api/search';
 const requestUrl = 'https://www.food2fork.com/api/get';
 
