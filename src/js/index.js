c6a1506eba353cf99dc78bd2076c0a2c8995e876
Lou Stringer <louise.stringer@gmail.com>
2019-02-12 22:32:56 +0000

    searchView: get query input, show recipe list in ui

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,21 @@
 // Controller file
 import Search from './models/Search';
 import { domElements } from './views/base';
-import * from './views/searchViews';
+import * as searchView from './views/searchView';
 
 // track global state (current search, selected recipe etc)
 const state = {};
 
 // Search for recipes
 const controlSearch = async () => {
-  const query = 'vegan pasta'; // get from search view
+  const query = searchView.getSearchQuery();
   if (query) {
     state.search = new Search(query);
     // add UI waiting signal
     await state.search.getResults();
-    console.log(state.search.recipes); // send to search view
+    searchView.showRecipes(state.search.recipes); 
   }
-}
+};
 
 domElements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
