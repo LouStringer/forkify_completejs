f309f2329e7572db4f8860c6948c4462d8ac201b
Lou Stringer <louise.stringer@gmail.com>
2019-02-12 21:35:01 +0000

    add base.js + obj to hold all DOM elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,23 @@
 // Controller file
 import Search from './models/Search';
+import { domElements } from './views/base';
+import * from './views/searchViews';
 
 // track global state (current search, selected recipe etc)
 const state = {};
 
 // Search for recipes
 const controlSearch = async () => {
-  const query = 'vegan pasta';
+  const query = 'vegan pasta'; // get from search view
   if (query) {
     state.search = new Search(query);
+    // add UI waiting signal
     await state.search.getResults();
-    console.log(state.search.recipes);
+    console.log(state.search.recipes); // send to search view
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+domElements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
---
+++ src/js/views/base.js
@@ -0,0 +1,4 @@
+export const domElements = {
+  searchForm: document.querySelector('.search'),
+  searchInput: document.querySelector('search__field'),
+}
