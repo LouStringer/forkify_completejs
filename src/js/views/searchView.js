a47bb877f98e4dd47b3924a52efb8f7f5a21702e
Lou Stringer <louise.stringer@gmail.com>
2019-02-12 22:55:01 +0000

    refactor searchView

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,19 +2,23 @@ import { domElements } from './base';
 
 export const getSearchQuery = () => domElements.searchInput.value;
 
+const prepRecipeHTML = (recipe) => {
+  const recipeHTML = `
+    <li>
+        <a class="results__link" href="#${recipe.recipe_id}">
+            <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="${recipe.title}">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${recipe.title}</h4>
+                <p class="results__author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>
+  `;
+  domElements.resultsList.insertAdjacentHTML('beforeend', recipeHTML);
+}
+
 export const showRecipes = (recipes) => {
-  const recipeList = recipes.map((item) => {
-    return `<li>
-      <a class="results__link results__link--active" href="#${item.recipe_id}">
-        <figure class="results__fig">
-          <img src="${item.image_url}" alt="the cooked dish for this recipe">
-        </figure>
-        <div class="results__data">
-          <h4 class="results__name">${item.title}</h4>
-          <p class="results__author">${item.publisher}</p>
-        </div>
-      </a>
-    </li>`;
-  });
-  domElements.resultsList.innerHTML = recipeList.join();
+  recipes.forEach(prepRecipeHTML);
 }
