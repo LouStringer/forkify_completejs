46127d525245a98504de1f12625775e7a2de2517
Lou Stringer <louise.stringer@gmail.com>
2019-02-13 19:19:33 +0000

    add loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 // Controller file
 import Search from './models/Search';
-import { domElements } from './views/base';
+import { domElements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 
 // track global state (current search, selected recipe etc)
@@ -11,10 +11,11 @@ const controlSearch = async () => {
   const query = searchView.getSearchQuery();
   if (query) {
     state.search = new Search(query);
-    // add UI waiting signal
+    renderLoader(domElements.results);
     searchView.clearSearchInput();
     searchView.clearRecipeList();
     await state.search.getResults();
+    clearLoader();
     searchView.showRecipes(state.search.recipes);
   }
 };
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,26 @@
 export const domElements = {
   searchForm: document.querySelector('.search'),
   searchInput: document.querySelector('.search__field'),
+  results: document.querySelector('.results'),
   resultsList: document.querySelector('.results__list'),
+};
+
+export const domElementStrings = {
+  loader: 'loader',
+};
+
+export const renderLoader = parent => {
+  const loader = `
+    <div class="${domElementStrings.loader}">
+      <svg>
+        <use href="img/icons.svg#icon-cw"></use>
+      </svg>
+    </div>
+  `;
+  parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${domElementStrings.loader}`);
+  loader.parentElement.removeChild(loader);
 }
